0c49b09f1333fd765fb0757dcd37a74b60eed542
Nicolai Erbs <nicolai.erbs@infomotion.de>
2020-10-20 22:03:39 +0200

    debug

--- disk_manager.py
+++ disk_manager.py
@@ -65,6 +65,7 @@ def main():
     delete_old_files(image_path + 'detection/', delete_delay_detection)
     delete_old_files(image_path + 'no_detection/', delete_delay_no_detection)
     current = latest_image(image_path)
+    print(current[1])
     last_image_timestamp = datetime.fromtimestamp(int(current[0])).strftime('%d.%m. %H:%M')
     content = 'Fahrstuhl Update: ' + get_disk_space() + ' frei bei ' + str(image_count(image_path))\
               + ' Bildern (letztes Bild um ' + last_image_timestamp + ')'
